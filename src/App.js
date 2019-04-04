443f7a922fe7434e6e46b17d29deecb31b6eb2d5
Steven Ringo <git@stevenringo.com>
2019-04-05 06:45:27 +1100

    working-ish

--- src/App.js
+++ src/App.js
@@ -12,18 +12,48 @@ class App extends Component {
 
   constructor(props) {
     super(props);
-    this.state = { votes: [] };
-  }
+    this.state = {
+      votes: [],
+      votes_new: {
+        "e1438020-bd8f-45c7-9d94-0e40e1eadadf": { counter: 0 },
+        "8e28f53b-7988-439e-bfa8-d2b429378cc0": { counter: 0 },
+        "2c7672d2-65ec-4a92-8aef-5d732dc2642d": { counter: 0 },
+        "0df14032-e1a4-48f4-be33-12aaf8706d0d": { counter: 0 }
+      }
+    };
+  };
 
   async componentDidMount() {
+    console.log("state before:", this.state)
     try {
       const votes = await API.graphql(graphqlOperation(queries.listVotes))
       this.setState({
         votes: votes.data.listVotes.items
       })
+      console.log("state:", this.state)
+      console.log("items", votes.data.listVotes.items);
     } catch (err) {
       console.log('error fetching votes...', err)
     }
+
+    const subscription = API.graphql(
+      graphqlOperation(subscriptions.onUpdateVote)
+    ).subscribe({
+      next: (updateData) => {
+        const id      = updateData.value.data.onUpdateVote.id
+        const counter = updateData.value.data.onUpdateVote.counter
+        console.log("id", id);
+        console.log("counter", counter);
+        let new_val = {"counter": counter }
+        // console.log("new val:", new_val);
+        var existing_state = {...this.state.votes_new}
+        existing_state[id] = new_val
+        this.setState({
+          votes_new: existing_state
+        });
+        console.log("state after:", this.state);
+      }
+    })
   }
 
   candidates() {
@@ -32,6 +62,7 @@ class App extends Component {
         key={candidate.id}
         id={candidate.id}
         name={candidate.candidate}
+        counter={this.state.votes_new[candidate.id].counter}
       />);
   }
 
@@ -45,12 +76,23 @@ class Candidate extends Component {
 
   handleSubmit = async (event) => {
     await event.preventDefault;
-    console.log(event);
-    return;
+    const castVote = {
+      id: event,
+      counter: Math.floor(Math.random() * Math.floor(1000))
+    };
+
+    const newEvent = await API.graphql(graphqlOperation(mutations.updateVote, {input: castVote}));
+    console.log(JSON.stringify(newEvent));
   };
 
   render() {
-    return <button className="ui button" onClick={() => this.handleSubmit(this.props.id)}>{this.props.name}</button>;
+    console.log("props", this.props)
+    return (
+      <div>
+        {this.props.counter}
+        <button className="ui button" onClick={() => this.handleSubmit(this.props.id)}>{this.props.name}</button>
+      </div>
+    );
   }
 }
 
