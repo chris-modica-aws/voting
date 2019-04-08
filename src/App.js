60b4aa167032c55b03a5083b25f27981f303f0f2
Steven Ringo <git@stevenringo.com>
2019-04-08 10:06:32 +1000

    Some changes...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { withAuthenticator } from 'aws-amplify-react';
 import Amplify, { API, graphqlOperation } from 'aws-amplify';
+import { Bar } from 'react-chartjs-2';
 import * as queries from './graphql/queries';
 import * as mutations from './graphql/mutations';
 import * as subscriptions from './graphql/subscriptions';
 import aws_exports from './aws-exports';
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
+import classNames from 'classnames';
+
 
 Amplify.configure(aws_exports);
 
@@ -46,25 +47,30 @@ class App extends Component {
   }
 
   candidates() {
-    return this.state.votes.map(candidate =>
+    const candidateColors = ["red", "orange", "green", "blue"]
+    return this.state.votes.map((candidate,idx) =>
       <Candidate
         key={candidate.id}
         id={candidate.id}
         name={candidate.candidate}
         counter={candidate.counter}
+        color={candidateColors[idx]}
       />
     )
   };
 
   render() {
-    var divStyle = {
-      width: '100%',
-      height: '600px'
-    };
     return (
-      <div className="App">
-        {this.candidates()}
-        <div style={divStyle}>
+      <div className="App flex flex-wrap">
+        <div className="w-full sm:w-2/5 px-3 pb-4" style={{backgroundColor: '#232F3E'}}>
+          <h1 className="text-grey-lighter text-xl my-6">Polling booth</h1>
+          <div className="text-grey-lighter italic my-6">Which is your favourite AWS serverless service?</div>
+          <div className="flex abg-grey-lighter pb-2">
+            {this.candidates()}
+          </div>
+        </div>
+        <div className="w-full sm:w-3/5 p-4">
+          <h1 className="text-xl text-grey-darkest pb-6">Live polling updates</h1>
           <Chart data={this.state.votes}></Chart>
         </div>
       </div>
@@ -74,71 +80,45 @@ class App extends Component {
 
 class Chart extends React.Component {
   render() {
-    const colors = ["#FC0107", "#FECC66", "#108001", "#0F80FF"]
-    const myData = this.props.data.map(
-      (vote,idx) => ({
-        candidate: vote.candidate,
-        counter: vote.counter,
-        color: colors[idx]
-      })
-    );
-
+    const fontStack = '-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"';
+    const data = {
+      labels: this.props.data.map((vote) => ( vote.candidate )),
+      datasets: [{
+        label: false,
+        data: this.props.data.map((vote) => ( vote.counter )),
+        backgroundColor: ['#CC1F1A', '#DE751F', '#1F9D55', '#2779BD']
+      }]
+    };
+    const options = {
+      title:    {display: false },
+      legend:   {display: false },
+      tooltips: {enabled: false },
+      layout:   {padding: {top: 10 }},
+      scales: {
+        xAxes: [{gridLines: {display: false }, ticks: {fontStyle: 'bold', fontColor: '#3D4852', fontFamily: fontStack}}],
+        yAxes: [{ticks: {beginAtZero: true, maxTicksLimit: 6, fontStyle: 'normal', fontColor: '#3D4852', fontFamily: fontStack}}]
+      }
+    }
     return (
-      <VictoryChart
-        // domainPadding will add space to each side of VictoryBar to
-        // prevent it from overlapping the axis
-        domainPadding={{x: [50, 10]}}
-        maxDomain={{ y: 100 }}
-        height={300}
-        width={300}
-        >
-        <VictoryAxis
-          style={{
-            axis: {stroke: "#756f6a"},
-            ticks: {stroke: "grey", size: 0},
-            tickLabels: {fontSize: 12, padding: 5, fontFamily: '-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'}
-          }}
-        />
-        <VictoryAxis
-          dependentAxis
-          style={{
-            axis: {stroke: "#756f6a"},
-            ticks: {stroke: "grey", size: 5},
-            tickLabels: {fontSize: 12, padding: 5, fontFamily: '-apple-system, system-ui, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"'}
-          }}
-        />
-        <VictoryBar
-          barRatio={1}
-          data={myData}
-          alignment="middle"
-          x="candidate"
-          y="counter"
-          style={{ data: {fill: (d) => d.color }}}
-        />
-      </VictoryChart>
-    )
+      <Bar data={data} width={100} height={50} options={options} />
+    );
   }
 }
 
 class Candidate extends Component {
-
   handleSubmit = async (event) => {
     await event.preventDefault;
     const castVote = {
       id: event.id
     };
-
     const newEvent = await API.graphql(graphqlOperation(mutations.castVote, {input: castVote}));
-    // console.log(JSON.stringify(newEvent));
   };
 
   render() {
     console.log("props", this.props)
+    const classes = classNames('flex-1', 'text-white', 'py-2', 'px-3', 'mx-1', 'text-sm', 'rounded', 'bg-' + this.props.color + '-dark', 'hover:bg-' + this.props.color + '-lighter');
     return (
-      <div>
-      {this.props.counter}
-      <button className="ui button" onClick={() => this.handleSubmit(this.props)}>{this.props.name}</button>
-      </div>
+      <button className={classes} onClick={() => this.handleSubmit(this.props)}>{this.props.name}</button>
     );
   }
 }
